01e00da6fc10723e1e619b23a7cb401b5250007c
GB_JG <shogun0331@gmail.com>
2022-01-28 17:42:52 +0900

    메타 파일 추가

--- Assets/Script/Etc/ObjectPooling/PoolingType.cs
+++ Assets/Script/Etc/ObjectPooling/PoolingType.cs
@@ -4,9 +4,6 @@ namespace GB
 {
     public class PoolingType : MonoBehaviour
     {
-        public int ID = 0;
         public string Name = string.Empty;
-
-        public bool Active = false;
     }
 }
